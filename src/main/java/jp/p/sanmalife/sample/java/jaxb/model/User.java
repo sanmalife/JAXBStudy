cd779397c9f3f6b52bc24424e994a14b097f62cb
Takeshi kozai <sanmalife@gmail.com>
2012-12-19 01:22:29 +0900

    空に関するテストの作成
    空文字列のテストが失敗している

--- src/main/java/jp/p/sanmalife/sample/java/jaxb/model/User.java
+++ src/main/java/jp/p/sanmalife/sample/java/jaxb/model/User.java
@@ -5,6 +5,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class User {
 
+    public String id;
     public String name;
     public int age;
 
--- src/test/java/jp/p/sanmalife/sample/java/jaxb/model/UserTest.java
+++ src/test/java/jp/p/sanmalife/sample/java/jaxb/model/UserTest.java
@@ -15,7 +15,7 @@ import org.junit.Test;
 
 public class UserTest {
 
-    private static final String USER_STR = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><user><name>Tom</name><age>15</age></user>";
+    private static final String USER_STR = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><user><id>0001</id><name>Tom</name><age>15</age></user>";
     private Marshaller m;
     private Unmarshaller um;
     private User tom;
@@ -27,19 +27,16 @@ public class UserTest {
         um = ctx.createUnmarshaller();
 
         tom = new User();
+        tom.id = "0001";
         tom.name = "Tom";
         tom.age = 15;
     }
 
     @Test
     public void ユーザオブジェクトをMarshalize出来る() throws Exception {
-        User user = new User();
-        user.name = "Tom";
-        user.age = 15;
-
         StringWriter sw = new StringWriter();
         try {
-            m.marshal(user, sw);
+            m.marshal(tom, sw);
             String userStr = sw.toString();
             assertThat(userStr, is(USER_STR));
         } finally {
@@ -52,6 +49,38 @@ public class UserTest {
         StringReader sr = new StringReader(USER_STR);
         User user = (User) um.unmarshal(sr);
         assertThat(user, is(tom));
+    }
+
+    @Test
+    public void nullオブジェクトはmarshalizeされない() throws Exception {
+        StringWriter sw = new StringWriter();
+        User u = new User();
+
+        String expect = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><user><age>0</age></user>";
+        try {
+            m.marshal(u, sw);
+            String str = sw.toString();
+            assertThat(str, is(expect));
+        } finally {
+            sw.close();
+        }
+    }
+
+    @Test
+    public void 空文字列は空要素としてmarshalizeされる() throws Exception {
+        StringWriter sw = new StringWriter();
+        User user = new User();
+        user.name = "";
+
+        String expect = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><user><name /><age>0</age></user>";
+        try {
+            m.marshal(user, sw);
+            String result = sw.toString();
+            assertThat(result, is(expect));
+        } finally {
+            sw.close();
+        }
+
     }
 
 }
